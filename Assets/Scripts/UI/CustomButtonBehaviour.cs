00750dfc0a34639c2e421ddc1ebaf1e8b2449b36
Phu Thanh <phuthanh.vo1379@gmail.com>
2022-11-11 22:41:41 +0700

    feat (assets, prefabs, scripts):
    - add related assets for RuleTile and AnimatedTile
    - clean unused codes

--- Assets/Scripts/UI/CustomButtonBehaviour.cs
+++ Assets/Scripts/UI/CustomButtonBehaviour.cs
@@ -8,17 +8,20 @@ public class CustomButtonBehaviour : MonoBehaviour, IPointerEnterHandler, IPoint
 
     public event Action OnCustomMouseEnterEvent;
     public Action OnCustomMouseExitEvent;
+    public ParticleSystem particleSystem;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         animator.SetBool("isHover", true);
         OnCustomMouseEnterEvent?.Invoke();
+        particleSystem.Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         animator.SetBool("isHover", false);
         OnCustomMouseExitEvent?.Invoke();
+        particleSystem.Stop();
     }
 
     public int ReturnInt(int a)
