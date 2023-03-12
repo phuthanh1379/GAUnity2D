3702ff118642f79aeb88e60fb189ac0f407fc409
Phu Thanh <phuthanh.vo1379@gmail.com>
2023-03-12 10:21:09 +0700

    delete (files):
    - remove M3 and TD

--- Assets/Scripts/MatchThree/CellData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "CellData", menuName = "MatchThree/CellData")]
-public class CellData : ScriptableObject
-{
-    public int value;
-    public Sprite sprite;
-}
